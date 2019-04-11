46d50f3507040c0c8ba1427d36cd08c390a0909e
Gerd <g.rardo1920@gmail.com>
2019-04-11 08:18:46 -0500

    con rutas y principio de lo de heroku

--- app/components/twitter-box.js
+++ app/components/twitter-box.js
@@ -4,6 +4,12 @@ import {action} from "@ember/object"
 
 export default class TwitterBoxComponent extends Component {
 	@tracked text;  //this.state.text
+	@tracked error;
+	@action
+	onInput(value){
+		this.error=false;
+		this.text=value;
+	}
 	@action handleChange (text)// everytime i tipe sth, this function is called
 	{	
 		//alert ("handleChage")
@@ -11,10 +17,12 @@ export default class TwitterBoxComponent extends Component {
 		console.log(text);
 		this.text= text;
 	}
-	@action handleSubmit (){ 
+	@action async handleSubmit (){ 
 		let newText=this.text;
 		if (this.args.onSubmit){  //sin parèntesis no ejecuta
-		this.args.onSubmit(newText); //args=props
+		let req= await  this.args.onSubmit(newText); //args=props    se va y queremos que luego regrese
+		this.error=req.error;
+		this.text= "";
 	}else
 		alert("HandleS");
 	}
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,19 @@
 import Controller from '@ember/controller';
 
 export default class ApplicationController extends Controller {
-	
+	@tracked tweets=[
+		{id:1, user_name: "Yvone", description:"hola,hola", created_at:"ok"}
+	];
+	onSubmit(description){
+		let tweets= this.tweets.slice();
+
+		this.tweets =tweets.concat({
+			id:Math.floor(Math.random()*100)+3,
+			user_name:"Yvone",
+			description:text,
+			created_at:"2019-04-01"
+		})
+	}
 }
 
 
